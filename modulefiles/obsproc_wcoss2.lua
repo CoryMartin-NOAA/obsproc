7e641d06cafbf84e27d5020733c67c279349ee3d
cory martin <cory.r.martin@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
2025-03-17 14:35:08 +0000

    use dev spack stack on wcoss2

--- modulefiles/obsproc_wcoss2.lua
+++ modulefiles/obsproc_wcoss2.lua
@@ -8,12 +8,14 @@ cmake_ver=os.getenv("cmake_ver")
 craype_ver=os.getenv("craype_ver")
 cray_mpich_ver=os.getenv("cray_mpich_ver")
 
+prepend_path("MODULEPATH", "/apps/ops/test/spack-stack-1.6.0-nco/envs/nco-intel-19.1.3.304/install/modulefiles/Core")
+
 -- load("envvar")
 load(pathJoin("PrgEnv-intel", PrgEnv_intel_ver))
-load(pathJoin("intel", intel_ver))
+load(pathJoin("stack-intel/19.1.3.304"))
+load(pathJoin("stack-cray-mpich/8.1.9"))
 load(pathJoin("cmake", cmake_ver))
 load(pathJoin("craype", craype_ver))
-load(pathJoin("cray-mpich", cray_mpich_ver))
 
 -- Load common modules for this package
 load("obsproc_common")
